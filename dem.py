b04dfaaff186da6019b25473d6b20d00a50cd431
Accamma-2 <accammabl@gmail.com>
2025-02-12 16:06:34 +0530

    done

---
+++ dem.py
@@ -0,0 +1,9 @@
+class student:
+    def __init(self,sname,sid,sadd):
+        self.name=sname
+        self.id=sid
+        self.add=sadd
+s1=student("ram",123,"blr")
+s2=student("shyam",234,"mysore")
+print(s1.name)
+print(s2.name)
