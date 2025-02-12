e0500706c873ea3d3b273c53791966870ec86de2
Accamma-2 <accammabl@gmail.com>
2025-02-12 16:10:53 +0530

    created an object s3 and printed the address

--- dem.py
+++ dem.py
@@ -5,6 +5,9 @@ class student:
         self.add=sadd
 s1=student("ram",123,"blr")
 s2=student("shyam",234,"mysore")
+s3=student("radha",456,"coorg")
 print(s1.name)
 print(s2.name)
 print(s1.add)
+print(s3.add)
+
